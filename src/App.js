37279ba3cb50bdc237dc0deb4b002a9d45078662
OzkanOzsahin <ozkanozsahin@hotmail.com>
2022-10-11 15:57:10 +0200

    barchart

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import Home from './Home'
-import Data from './src/Data'
-import BarChart from './Components/Barchart'
+//import BarChart from './BarChart';
+//import { BrowserRouter,  Route } from 'react-router-dom'
+
+import Data from './Data'
 import { VictoryChart, VictoryLine, VictoryTheme } from 'victory';
 
 
 function App() {
+  
   return (
     <div className="container-app">
       
-        <BrowserRouter>
-        <Route>
+        
        <VictoryChart
        theme={VictoryTheme.material}
        >
@@ -20,14 +20,16 @@ function App() {
         style={{data: {stroke:"black"},
         parent: { border:"1px solid #ccc"}
   }}
+  
   data={[Data]}
     
       />
       </VictoryChart>
-      </Route>
-      </BrowserRouter>
+      
+      
     </div>
-  
+  )
+}
 
 
 export default App;
---
+++ src/Components/LineChart.js
@@ -0,0 +1,7 @@
+import React from "react";
+import {VictoryChart, VictoryAxis, VictoryLine} from "victory"
+
+function LineChart({Data}) {
+averageRating = Data.map(assignment =>({
+        assignment:
+}
